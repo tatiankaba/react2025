730f74328c629f26152700908e5d3d2498b34a94
Tatiana <bodtata@yandex.ru>
2025-07-13 16:14:12 +0200

    test husky

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div>ааа
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
